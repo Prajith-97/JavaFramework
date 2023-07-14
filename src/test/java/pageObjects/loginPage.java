fcdd5c70352c4a3484d463b7bd6f24480ba95712
Prajith-97 <58196671+Prajith-97@users.noreply.github.com>
2023-07-14 16:34:35 +0530

    Revert "Feature/second"

--- src/test/java/pageObjects/loginPage.java
+++
@@ -1,7 +0,0 @@
-package pageObjects;
-
-public class loginPage {
-    public static String textBox_userName = "//input[@id='user-name']";
-    public static String textBox_password = "//input[@id='password']";
-    public static String login_button = "//input[@id='login-button']";
-}
--- target/test-classes/features/sample.feature
+++
@@ -1,5 +0,0 @@
-Feature: Sample Page
-  Scenario: Validate login with valid credential
-    Given user is on homepage
-    And user enter valid username "standard_user"
-    And user enter valid password "secret_sauce"
